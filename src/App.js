9799e318d8b3e01f50633bbe228e3f5f28b0a7f0
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-10-27 22:12:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,178 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+/**
+ * Triangle points
+ * - 50,16 85,85 15,85 50,16
+ * - 160,126 195,195 125,195 160,126
+ * - 50,126 85,195 15,195 50,126
+ * - 160,16 195,85 125,85 160,16
+ */
+
+// Position = 'lefttop', 'righttop', 'leftbottom', 'rightbottom'
+
+function Triangle({ position, isAnswer, ...rest }) {
+  const points = {
+    lefttop: "50,16 85,85 15,85 50,16",
+    righttop: "160,16 195,85 125,85 160,16",
+    leftbottom: "50,126 85,195 15,195 50,126",
+    rightbottom: "160,126 195,195 125,195 160,126"
+  };
+
+  const strokes = isAnswer
+    ? {
+        strokeWidth: "1",
+        stroke: "gray"
+      }
+    : {
+        strokeWidth: "5",
+        stroke: "red"
+      };
+
+  return <polygon points={points[position]} {...strokes} {...rest} />;
+}
+
+function Circle({ position, isAnswer, ...rest }) {
+  const coords = {
+    lefttop: { x: 50, y: 50 },
+    righttop: { x: 160, y: 50 },
+    leftbottom: { x: 50, y: 160 },
+    rightbottom: { x: 160, y: 160 }
+  };
+
+  const strokes = isAnswer
+    ? {
+        strokeWidth: "1",
+        stroke: "gray"
+      }
+    : {
+        strokeWidth: "5",
+        stroke: "red"
+      };
+
+  return (
+    <circle
+      cx={coords[position].x}
+      cy={coords[position].y}
+      r="35"
+      {...strokes}
+      {...rest}
+    />
+  );
+}
+
+function Rectangle({ position, isAnswer, ...rest }) {
+  const coords = {
+    lefttop: { x: 15, y: 15 },
+    righttop: { x: 125, y: 15 },
+    leftbottom: { x: 15, y: 125 },
+    rightbottom: { x: 125, y: 125 }
+  };
+
+  const strokes = isAnswer
+    ? {
+        strokeWidth: "1",
+        stroke: "gray"
+      }
+    : {
+        strokeWidth: "5",
+        stroke: "red"
+      };
+
+  return (
+    <rect
+      x={coords[position].x}
+      y={coords[position].y}
+      width="70"
+      height="70"
+      {...strokes}
+      {...rest}
+    />
+  );
+}
+
+const noop = () => null;
+
+export default function App() {
+  const [status, setStatus] = useState("");
+
+  const expectedShapes = {
+    LeftTop: Circle,
+    RightTop: Rectangle,
+    LeftBottom: Triangle,
+    RightBottom: noop
+  };
+
+  const [actualShapes, setActualShapes] = useState({
+    LeftTop: Rectangle,
+    RightTop: Triangle,
+    LeftBottom: Circle,
+    RightBottom: noop
+  });
+
+  function move(from) {
+    const newActualShapes = { ...actualShapes };
+
+    const available = Object.keys(actualShapes).find(
+      (key) => actualShapes[key] === noop
+    );
+    newActualShapes[available] = newActualShapes[from];
+    newActualShapes[from] = noop;
+
+    setActualShapes(newActualShapes);
+  }
+
+  function checkAnswer() {
+    const correct =
+      actualShapes.LeftTop === expectedShapes.LeftTop &&
+      actualShapes.RightTop === expectedShapes.RightTop &&
+      actualShapes.LeftBottom === expectedShapes.LeftBottom &&
+      actualShapes.RightBottom === expectedShapes.RightBottom;
+    setStatus(correct ? "Correct!" : "Incorrect");
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="transparent"
+          width="600"
+          height="250"
+        >
+          <rect x="0" y="0" width="100" height="100" fill="#efefef" />
+          <expectedShapes.LeftTop isAnswer position="lefttop" />
+          <actualShapes.LeftTop
+            position="lefttop"
+            onClick={() => move("LeftTop")}
+          />
+
+          <rect x="110" y="0" width="100" height="100" fill="#efefef" />
+          <expectedShapes.RightTop isAnswer position="righttop" />
+          <actualShapes.RightTop
+            position="righttop"
+            onClick={() => move("RightTop")}
+          />
+
+          <rect x="0" y="110" width="100" height="100" fill="#efefef" />
+          <expectedShapes.LeftBottom isAnswer position="leftbottom" />
+          <actualShapes.LeftBottom
+            position="leftbottom"
+            onClick={() => move("LeftBottom")}
+          />
+
+          <rect x="110" y="110" width="100" height="100" fill="#efefef" />
+          <expectedShapes.RightBottom isAnswer position="rightbottom" />
+          <actualShapes.RightBottom
+            position="rightbottom"
+            onClick={() => move("RightBottom")}
+          />
+        </svg>
+      </div>
+      <div>
+        <h1>{status}</h1>
+        <button onClick={checkAnswer}>Done</button>
+      </div>
+    </div>
+  );
+}
